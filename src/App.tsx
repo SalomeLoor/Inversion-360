35a4d1db6110078313623a3c578e9751d89a10e8
Salome Loor <mariasalomeloorvelez@gmail.com>
2026-07-11 17:27:07 -0500

    Estructura incial de carpetas

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+  
+
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
